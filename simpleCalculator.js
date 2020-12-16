1eb4101b2633d6b5537b32aa858020ef618a6b45
NilGamer <35883547+NilGamer@users.noreply.github.com>
2020-12-16 13:16:41 +0530

    Add files via upload

--- simpleCalculator.js
+++ simpleCalculator.js
@@ -1,3 +1,37 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
-export default class SimpleCalculator extends LightningElement {}
+export default class SimpleCalculator extends LightningElement {
+  @track result = '';
+  firstNumber = '';
+  secondNumber = '';
+
+  numberChangeHandler(event) {
+    if (event.target.name === 'firstNumber') {
+      console.log(event.target.value);
+      this.firstNumber = event.target.value;
+    } else {
+      this.secondNumber = event.target.value;
+    }
+  }
+
+  addHandler = () => {
+    this.result = `The result is ${
+      parseInt(this.firstNumber) + parseInt(this.secondNumber)
+    }`;
+  };
+  subHandler = () => {
+    this.result = `The result is ${
+      parseInt(this.firstNumber) - parseInt(this.secondNumber)
+    }`;
+  };
+  mulHandler = () => {
+    this.result = `The result is ${
+      parseInt(this.firstNumber) * parseInt(this.secondNumber)
+    }`;
+  };
+  divHandler = () => {
+    this.result = `The result is ${
+      parseInt(this.firstNumber) / parseInt(this.secondNumber)
+    }`;
+  };
+}
